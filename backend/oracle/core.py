788b766adc4048ed683385b1a0a874a85d1618a4
jp <wongjingping@gmail.com>
2024-08-13 15:03:36 +0800

    - return report_id in `/oracle/begin_generation` response - shift creation of new report metadata from the celery task to the router's method so that we have the report_id to return - lint

--- backend/oracle/core.py
+++ backend/oracle/core.py
@@ -6,30 +6,33 @@ from datetime import datetime
 from typing import Any, Dict
 
 from db_utils import OracleReports, engine
-from sqlalchemy import insert, select
+from sqlalchemy import select
 from sqlalchemy.orm import Session
 
 from .celery_app import celery_app
 
 celery_async_executors = ThreadPoolExecutor(max_workers=4)
 
+
 @celery_app.task
-def begin_generation_task(api_key: str, username: str, inputs: Dict[str, Any]):
+def begin_generation_task(
+    api_key: str, username: str, report_id: int, inputs: Dict[str, Any]
+):
     t_start = datetime.now()
     print(f"Starting celery task for {username} at {t_start}")
     with celery_async_executors:
         loop = asyncio.get_event_loop()
-        task = loop.create_task(begin_generation_async_task(api_key, username, inputs))
+        task = loop.create_task(
+            begin_generation_async_task(api_key, username, report_id, inputs)
+        )
         loop.run_until_complete(task)
     t_end = datetime.now()
     time_elapsed = (t_end - t_start).total_seconds()
     print(f"Completed celery task for {username} in {time_elapsed:.2f} seconds.")
 
-    
+
 async def begin_generation_async_task(
-    api_key: str,
-    username: str,
-    inputs: Dict[str, Any]
+    api_key: str, username: str, report_id: int, inputs: Dict[str, Any]
 ):
     """
     This is the entry point for the oracle, which will kick off the control flow,
@@ -41,23 +44,7 @@ async def begin_generation_async_task(
     Every call to control is scoped over a single report_id, which can only
     belong to 1 api_key and username.
     """
-    # insert a new row into the OracleReports table and get a new report_id
-    with Session(engine) as session:
-        stmt = (
-            insert(OracleReports)
-            .values(
-                api_key=api_key,
-                username=username,
-                inputs=inputs,
-                status="started",
-                created_ts=datetime.now()
-            )
-            .returning(OracleReports.report_id)
-        )
-        result = session.execute(stmt)
-        report_id = result.scalar_one()
-        session.commit()
-    
+
     stage = "gather_context"
     outputs = {}
     continue_generation = True
@@ -70,15 +57,12 @@ async def begin_generation_async_task(
                 report_id=report_id,
                 stage=stage,
                 inputs=inputs,
-                outputs=outputs
+                outputs=outputs,
             )
             outputs[stage] = stage_result
             # update the status and current outputs of the report generation
             with Session(engine) as session:
-                stmt = (
-                    select(OracleReports)
-                    .where(OracleReports.report_id == report_id)
-                )
+                stmt = select(OracleReports).where(OracleReports.report_id == report_id)
                 result = session.execute(stmt)
                 report = result.scalar_one()
                 report.status = stage
@@ -93,16 +77,14 @@ async def begin_generation_async_task(
         except Exception as e:
             # update the status of the report
             with Session(engine) as session:
-                stmt = (
-                    select(OracleReports)
-                    .where(OracleReports.report_id == report_id)
-                )
+                stmt = select(OracleReports).where(OracleReports.report_id == report_id)
                 result = session.execute(stmt)
                 report = result.scalar_one()
                 report.status = "error"
                 print(f"Error occurred in stage {stage}:\n{e}")
                 session.commit()
             continue_generation = False
+
 
 def next_stage(stage: str) -> str:
     if stage == "gather_context":
@@ -127,7 +109,7 @@ async def execute_stage(
     report_id: str,
     stage: str,
     inputs: Dict[str, Any],
-    outputs: Dict[str, Any]
+    outputs: Dict[str, Any],
 ):
     """
     Depending on the current stage, the control function will call the appropriate
@@ -135,11 +117,15 @@ async def execute_stage(
     actions to complete the stage and return the result.
     """
     if stage == "gather_context":
-        stage_result = await gather_context(api_key, username, report_id, inputs, outputs)
+        stage_result = await gather_context(
+            api_key, username, report_id, inputs, outputs
+        )
     elif stage == "explore":
         stage_result = await explore_data(api_key, username, report_id, inputs, outputs)
     elif stage == "wait_clarifications":
-        stage_result = await wait_clarifications(api_key, username, report_id, inputs, outputs)
+        stage_result = await wait_clarifications(
+            api_key, username, report_id, inputs, outputs
+        )
     elif stage == "predict":
         stage_result = await predict(api_key, username, report_id, inputs, outputs)
     elif stage == "optimize":
@@ -154,12 +140,13 @@ async def execute_stage(
         raise ValueError(f"Stage {stage} not recognized.")
     return stage_result
 
+
 async def gather_context(
     api_key: str,
     username: str,
     report_id: str,
     inputs: Dict[str, Any],
-    outputs: Dict[str, Any]
+    outputs: Dict[str, Any],
 ):
     """
     This function will gather the context for the report, by consolidating
@@ -176,12 +163,13 @@ async def gather_context(
     await sleep(random.randint(1, 5))
     return {"context": "context gathered"}
 
+
 async def explore_data(
     api_key: str,
     username: str,
     report_id: str,
     inputs: Dict[str, Any],
-    outputs: Dict[str, Any]
+    outputs: Dict[str, Any],
 ):
     """
     This function will explore the data, by generating a series of exploratory
@@ -194,16 +182,17 @@ async def explore_data(
     # dummy print statement for now
     print(f"Exploring data for report {report_id}")
 
+
 async def wait_clarifications(
     api_key: str,
     username: str,
     report_id: str,
     inputs: Dict[str, Any],
-    outputs: Dict[str, Any]
+    outputs: Dict[str, Any],
 ):
     """
     This function will check the `clarifications` table in the SQLite3 database,
-    polling every x seconds to see if all the clarifications for a given 
+    polling every x seconds to see if all the clarifications for a given
     `report_id` have been addressed before proceeding to the next stage.
     """
     # TODO implement this function
@@ -213,12 +202,13 @@ async def wait_clarifications(
     await sleep(random.randint(1, 5))
     return {"clarifications": "all clarifications addressed"}
 
+
 async def predict(
     api_key: str,
     username: str,
     report_id: str,
     inputs: Dict[str, Any],
-    outputs: Dict[str, Any]
+    outputs: Dict[str, Any],
 ):
     """
     This function will make the necessary predictions, by training a machine learning
@@ -233,12 +223,13 @@ async def predict(
     await sleep(random.randint(1, 5))
     return {"predictions": "predictions generated"}
 
+
 async def optimize(
     api_key: str,
     username: str,
     report_id: str,
     inputs: Dict[str, Any],
-    outputs: Dict[str, Any]
+    outputs: Dict[str, Any],
 ):
     """
     This function will optimize the objective input by the user, considering
@@ -253,12 +244,13 @@ async def optimize(
     await sleep(random.randint(1, 5))
     return {"optimization": "optimization completed"}
 
+
 async def export(
     api_key: str,
     username: str,
     report_id: str,
     inputs: Dict[str, Any],
-    outputs: Dict[str, Any]
+    outputs: Dict[str, Any],
 ):
     """
     This function will export the final report, by consolidating all the
